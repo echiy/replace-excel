83695d14ea1963763303938a66792a5db1098b15
firsttraining <t9511015@gmail.com>
2024-05-15 09:59:31 -0400

    first upload

---
+++ src/Spin/Spin.js
@@ -0,0 +1,21 @@
+import './spin.css';
+
+function FastSpin(){
+    return(
+        <>
+
+
+        <div>
+            <div className='spinner'>
+
+            </div>
+            <p className='sam_abs' style={{color:'green', fontSize:'15px', wordSpacing:'4px', letterSpacing:'2px'}}>Setting up now...</p>
+
+        </div>
+
+
+        </>
+    )
+};
+
+export default FastSpin;
